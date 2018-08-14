f7bb1aab176e699f5374c60cd4d5665128a3ae77
Javier Olaechea <pirata@gmail.com>
2018-08-14 18:54:27 -0500

    [scratch]: For the current iteration

--- scratch.el
+++ scratch.el
@@ -17,3 +17,35 @@
     ;; (switch-to-buffer (current-buffer))
     (pivotal-insert-projects pivotal-projects-test-data)))
 
+
+(defvar pivotal-iterations-test-data)
+(pivotal-json-api (pivotal-v5-url "projects"
+                                  (number-to-string
+                                   (cdr (assoc 'id
+                                               (aref pivotal-projects-test-data 0))))
+                                  "iterations" "?scope=current")
+                  "GET"
+                  (lambda (status)
+                    (message "Status: %s" status)
+                    (let ((json (progn
+                                  (goto-char url-http-end-of-headers)
+                                  (json-read))))
+                      (setq pivotal-iteration-test-data
+                            json)
+                      json)))
+
+(with-current-buffer (get-buffer-create "*pivotal-iteration*")
+  (let ((inhibit-read-only t))
+    (pivotal-mode)
+    (erase-buffer)
+    ;; (switch-to-buffer (current-buffer))
+    (pivotal-insert-iteration pivotal-iterations-test-data)))
+
+(let ((iteration (aref pivotal-iterations-test-data 0)))
+  (values (assoc 'start iteration)
+          (assoc 'finish iteration)))
+
+(let ((stories (cdr (assoc 'stories (aref pivotal-iterations-test-data 0)))))
+  (assoc 'current_state (aref stories 0)))
+(length pivotal-iterations-test-data)
+;; Test for non-existent iterations
